c0a95aa650fe8a69028e64d2e62a9b511a9fc131
Fahim <fahim.private@gmail.com>
2026-04-27 11:49:07 -0400

    chore(tailwind): migrate config to TypeScript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -36,4 +38,6 @@ export default {
     },
   },
   plugins: [],
-}
+};
+
+export default config;
